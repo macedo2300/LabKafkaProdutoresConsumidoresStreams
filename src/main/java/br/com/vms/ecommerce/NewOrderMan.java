02ae8cfea1a32bb2fc50aa61d3f5463beebd6906
Vagner Macedo <macedo2300@gmail.com>
2021-01-25 19:05:42 -0300

    add o projeto kafka

---
+++ src/main/java/br/com/vms/ecommerce/NewOrderMan.java
@@ -0,0 +1,36 @@
+package br.com.vms.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMan {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        var producer = new KafkaProducer<String,String>(properties());
+        var value = "741258,01478520,1563";
+        var record = new ProducerRecord("ECOMMERCE_NEW_ORDER",value,value);
+        producer.send(record,(data, ex) -> {
+            if(ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("sucesso enviado "+data.topic()+ "::: partition:"+ data.partition()+"/ offset:"+data.offset()+"/ timestamp:"+data.timestamp());
+        }).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
